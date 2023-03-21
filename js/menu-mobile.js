2348ee292ab2b1f4b09051215cf9394e835914f9
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-03-21 11:48:17 -0300

    Ajustes

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,28 +1,27 @@
-const menuMobile = document.querySelector(".js-menu-mobile");
+const menu = () => {
+    const menuMobile = document.querySelector(".js-menu-mobile");
+    const menuLinks = document.querySelectorAll(".js-menu-link");
+    
+        menuMobile.addEventListener("click", () => {
+            navItens.classList.toggle("ativo");
+        });
+    
+        menuLinks.forEach((link) => {   
+            link.addEventListener("click", () => {
+                if(link.id == "menu-inicio" || link.id == "menu-radioweb" || link.id == "menu-programacao" || link.id == "menu-locutores" || link.id == "menu-contato") {
+                    navItens.classList.remove("ativo");
+                };
+            });
+        });
+    
+        const navItens = document.querySelector(".js-menu-itens");
+        navItens.addEventListener("click", () => {
+            navItens.classList.remove("ativo");
+        });
+}    
 
-const menuLinks = document.querySelectorAll(".js-menu-link");
+menu();
 
-
-console.log(menuLinks);
-
-    menuMobile.addEventListener("click", () => {
-        navItens.classList.toggle("ativo");
-    });
-
-    menuLinks.forEach((link) => {   
-        link.addEventListener("click", () => {
-            if(link.id == "menu-inicio" || link.id == "menu-radioweb" || link.id == "menu-programacao" || link.id == "menu-locutores" || link.id == "menu-contato") {
-                navItens.classList.remove("ativo");
-            }
-        })
-    })
-
-    const navItens = document.querySelector(".js-menu-itens");
-    navItens.addEventListener("click", () => {
-        navItens.classList.remove("ativo")
-    })
-
-   
 
     
 
