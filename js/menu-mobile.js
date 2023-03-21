f88d67e72cf980252f2003cb575695039e5eb14c
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-03-21 08:52:45 -0300

    Ajustes

--- js/menu-mobile.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".js-menu-mobile");
-const menuItens = document.querySelector(".js-menu-itens")
-
-menu.addEventListener("click", () => {
-    menuItens.classList.toggle("ativo")
-});
