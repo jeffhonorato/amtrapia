40c21c786061f9d5d96c3dbe6116c309b26f7fea
Jefferson Honorato <jeffhonoratobarros@gmail.com>
2023-03-21 12:11:02 -0300

    Ajustes

---
+++ js/swiper-programacao.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".swiper-programacao", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      rotate: 10,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
